4602f5ff65035840993f58dc13b63b889b14b4dd
Siegfried Boeken <37549326+VarVergErgos@users.noreply.github.com>
2023-01-12 19:44:56 +0100

    created a dialog from DialogComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {MatDialog, MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
+import { DialogComponent } from './dialog/dialog.component';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular-Kurs-Mat';
+
+  constructor (private dialog: MatDialog){
+
+  }
+
+  openDialog() {
+    this.dialog.open(DialogComponent, {
+    
+    });
+
+  }
 }
