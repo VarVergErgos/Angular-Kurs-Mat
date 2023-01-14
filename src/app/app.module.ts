7bff16397a51e55cd69b6ce1250e46cac50782f3
Siegfried Boeken <37549326+VarVergErgos@users.noreply.github.com>
2023-01-14 12:49:55 +0100

    create the price input field and the comment as textarea

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-/**
- * MATERIAL DESIGN
- */
+/** MATERIAL DESIGN */
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon'; 
 import { MatButtonModule } from '@angular/material/button'; 
@@ -19,7 +16,6 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatDatepickerModule } from '@angular/material/datepicker'; 
 import { MatNativeDateModule} from '@angular/material/core'
 import { MatRadioModule } from '@angular/material/radio'; 
-
 
 
 @NgModule({
